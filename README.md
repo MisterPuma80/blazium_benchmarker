b79be1ee0cd3dac00fd12a261b42a068cbc0ea72
Mister Puma <MisterPuma80@gmail.com>
2026-02-16 17:25:41 -0800

    Benchmarked functions that use _find_upper and _find_lower.

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
 # blazium_benchmarker
 
-# Simple Example
-```sh
-# Checkout repo
-git clone https://github.com/MisterPuma80/blazium_benchmarker
-cd blazium_benchmarker
-git checkout master
-
-# Download engines
-./benchmarker.sh download
-
-# Modify the engine code at example_blazium_4.5_modified/blazium ...
-# Add your benchmarks to example_blazium_4.5_modified/src/gdexample.cpp ...
-
-# Build everything and run benchmarks
-./benchmarker.sh engine benchmarks run show
-
-```
-
-# Benchmark Blazium PRs
+# Benchmark Blazium PR
 ```sh
 # Checkout repo
 git clone https://github.com/MisterPuma80/blazium_benchmarker
@@ -29,12 +11,12 @@ cd blazium_benchmarker
 
 
 # Run patch benchmark
-git checkout blazium_patch_597
+git checkout benchmark_to_upper_to_lower
 ./benchmarker.sh reset patch:597 engine benchmarks run show
 
-# Run different patch benchmark
-git checkout blazium_patch_602
-./benchmarker.sh reset patch:602 cores:10 linker:mold use_llvm:yes engine benchmarks run show
+# Run patch benchmark with clang and mold
+git checkout benchmark_to_upper_to_lower
+./benchmarker.sh reset linker:mold use_llvm:yes patch:597 engine benchmarks run show
 ```
 
 # Requirements
--- example_blazium_4.5_modified/src/gdexample.cpp
+++ example_blazium_4.5_modified/src/gdexample.cpp
@@ -43,30 +43,6 @@ using namespace godot;
 #include <godot_cpp/classes/random_number_generator.hpp>
 #include <godot_cpp/classes/scene_tree.hpp>
 
-Dictionary _test_node_get_children() {
-	uint64_t ticks = 0, total = 0, average = 0;
-	auto start = get_ticks_now();
-	auto end = get_ticks_now();
-	const int total_child_nodes = 100;
-	Node *root_tree = _make_node_tree_flat(total_child_nodes, 42);
-	for (int i = 0; i < ITERATIONS; i++) {
-		start = get_ticks_now();
-		TypedArray<Node> all = root_tree->get_children();
-		end = get_ticks_now();
-		ticks = get_ticks_diff(end, start);
-		total += ticks;
-		ASSERT_EQUAL(all.size(), total_child_nodes);
-	}
-	_delete_node_tree(root_tree);
-	average = total / ITERATIONS;
-
-	Dictionary values;
-	values[String("total")] = total;
-	values[String("average")] = average;
-	Dictionary entry;
-	entry[String("Node::get_children")] = values;
-	return entry;
-}
 
 Dictionary _test_string_to_lower() {
 	uint64_t ticks = 0, total = 0, average = 0;
@@ -116,13 +92,358 @@ Dictionary _test_string_to_upper() {
 	return entry;
 }
 
+
+
+
+
+
+
+Dictionary _test_string_findn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "No Place Like Like Home";
+	String to_find = "LIKE";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		int ret = a.findn(to_find);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, 9);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::findn")] = values;
+	return entry;
+}
+
+Dictionary _test_string_containsn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "No Place Like Like Home";
+	String to_find = "LIKE";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		bool ret = a.containsn(to_find);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, true);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::containsn")] = values;
+	return entry;
+}
+
+Dictionary _test_string_rfindn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "No Place Like Like Home";
+	String to_find = "LIKE";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		int ret = a.rfindn(to_find);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, 14);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::rfindn")] = values;
+	return entry;
+}
+
+Dictionary _test_string_matchn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no place like home";
+	String b = "NO ***** LIKE HOME";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		bool ret = a.matchn(b);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, true);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::matchn")] = values;
+	return entry;
+}
+
+Dictionary _test_string_is_subsequence_ofn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "abc";
+	String b = "AxByC";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		bool ret = a.is_subsequence_ofn(b);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, true);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::is_subsequence_ofn")] = values;
+	return entry;
+}
+
+
+
+
+Dictionary _test_string_nocasecmp_to() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no place like like home";
+	String b = "no place like like the circus";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		int ret = a.nocasecmp_to(b);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, -1);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::nocasecmp_to")] = values;
+	return entry;
+}
+
+Dictionary _test_string_naturalnocasecmp_to() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no place like like home";
+	String b = "no place like like the circus";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		int ret = a.naturalnocasecmp_to(b);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, -1);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::naturalnocasecmp_to")] = values;
+	return entry;
+}
+
+Dictionary _test_string_countn() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no place like like home";
+	String b = "LIKE";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		int ret = a.countn(b);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, 2);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::countn")] = values;
+	return entry;
+}
+
+Dictionary _test_string_replacen() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no place like like home";
+	String b = "LIKE";
+	String c = "LiKe";
+	String d = "no place LiKe LiKe home";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		String ret = a.replacen(b, c);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, d);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::replacen")] = values;
+	return entry;
+}
+
+
+
+
+Dictionary _test_string_to_camel_case() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no_place_like_like_home";
+	String b = "noPlaceLikeLikeHome";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		String ret = a.to_camel_case();
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, b);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::to_camel_case")] = values;
+	return entry;
+}
+
+Dictionary _test_string_to_pascal_case() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no_place_like_like_home";
+	String b = "NoPlaceLikeLikeHome";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		String ret = a.to_pascal_case();
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, b);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::to_pascal_case")] = values;
+	return entry;
+}
+
+Dictionary _test_string_capitalize() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "no_place_like_like_home";
+	String b = "No Place Like Like Home";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		String ret = a.capitalize();
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, b);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("String::capitalize")] = values;
+	return entry;
+}
+
+
+
+
+
+Dictionary _test_os_find_keycode_from_string() {
+	uint64_t ticks = 0, total = 0, average = 0;
+	auto start = get_ticks_now();
+	auto end = get_ticks_now();
+	String a = "Escape";
+	Key b = Key::KEY_ESCAPE;//"4194305";
+	for (int i = 0; i < ITERATIONS; i++) {
+		start = get_ticks_now();
+		Key ret = OS::get_singleton()->find_keycode_from_string(a);
+		end = get_ticks_now();
+		ticks = get_ticks_diff(end, start);
+		total += ticks;
+		ASSERT_EQUAL(ret, b);
+	}
+	average = total / ITERATIONS;
+
+	Dictionary values;
+	values[String("total")] = total;
+	values[String("average")] = average;
+	Dictionary entry;
+	entry[String("OS::find_keycode_from_string")] = values;
+	return entry;
+}
+
 void GDExample::_ready() {
 	Dictionary result;
 
-	result.merge(_test_node_get_children());
-
 	result.merge(_test_string_to_lower());
 	result.merge(_test_string_to_upper());
+
+	result.merge(_test_string_findn());
+	result.merge(_test_string_containsn());
+	result.merge(_test_string_rfindn());
+	result.merge(_test_string_matchn());
+	result.merge(_test_string_is_subsequence_ofn());
+
+	result.merge(_test_string_nocasecmp_to());
+	result.merge(_test_string_naturalnocasecmp_to());
+	result.merge(_test_string_countn());
+	result.merge(_test_string_replacen());
+
+	result.merge(_test_string_to_camel_case());
+	result.merge(_test_string_to_pascal_case());
+	result.merge(_test_string_capitalize());
+
+	result.merge(_test_os_find_keycode_from_string());
 
 	benchmark_footer(this, result);
 }
